1a7a9ca86ff3dfe582ad30c614378788da070361
Dangggchris <christophermtdang@gmail.com>
2019-06-16 18:19:08 -0700

    removed script link in htmls

--- server.js
+++ server.js
@@ -1,12 +1,5 @@
 var express = require("express");
 
-// var jsdom = require("jsdom");
-// const { JSDOM } = jsdom;
-// const { window } = new JSDOM();
-// const { document } = (new JSDOM('')).window;
-// global.document = document;
-// var $ = jQuery = require('jquery')(window);
-
 var PORT = process.env.PORT || 8080;
 var app = express();
 
