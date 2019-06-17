5814549ccd7926ef81fec2d10860b03797717585
Dangggchris <christophermtdang@gmail.com>
2019-06-16 17:54:40 -0700

    added function to save submitted data into friends API as JSON

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require("express");
 
-var PORT = process.env.PORT || 8080;
+// var jsdom = require("jsdom");
+// const { JSDOM } = jsdom;
+// const { window } = new JSDOM();
+// const { document } = (new JSDOM('')).window;
+// global.document = document;
+// var $ = jQuery = require('jquery')(window);
 
+var PORT = process.env.PORT || 8080;
 var app = express();
 
+// Routing  
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -13,4 +20,5 @@ require("./app/routing/htmlRoutes")(app);
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
   });
+  
   
