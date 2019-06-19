aab418f33f3525b7df36c2b1d2895c3ed537b570
Dangggchris <christophermtdang@gmail.com>
2019-06-19 02:00:47 -0700

    added difference calculation to find match

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,30 @@
-var friendArray = [];
+var friends = [
+    {
+        name: "Michael Scott",
+        photo: "https://cdn-images-1.medium.com/max/1600/1*oVdFZgDlATawsd3aVlReiw.jpeg",
+        scores: [1,4,3,5,2,4,3,2,4,3]
+    },
+    {
+        name: "Dwight Schrute",
+        photo: "https://pbs.twimg.com/profile_images/1863312401/Dwight_400x400.jpg",
+        scores: [3,5,1,2,4,3,2,1,5,3]
+    },
+    {
+        name: "Pam Beesely",
+        photo: "https://static2.srcdn.com/wordpress/wp-content/uploads/2018/10/The-Office-Pam-Beasley-Halpert.jpg?q=50&fit=crop&w=798&h=407&dpr=1.5",
+        scores: [3,5,2,4,3,1,4,2,5,2]
+    },
+    {
+        name: "Jim Halpert",
+        photo: "https://cdn-images-1.medium.com/max/1600/0*Xz-_cHSO6txphvHt.png",
+        scores: [4,3,5,2,4,3,1,2,4,5]
+    },
+    {
+        name: "Andy Bernard",
+        photo: "http://atlantablackstar.com/wp-content/uploads/2011/09/Andy-Bernard-e1318612354948.jpg",
+        scores: [4,2,1,2,3,2,5,4,3,4]
+    }
+];
 
-module.exports = friendArray;
+module.exports = friends;
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,9 +7,40 @@ module.exports = function(app) {
     })
 
     app.post("/api/friends", function(req,res) {
-        friendData.push(req.body);
-        res.json(true);
+
+        var newFriend = req.body;
+        var newSum = 0;
+        for (i = 0; i < newFriend.scores.length; i++) {
+            newSum += parseInt(newFriend.scores[i]);
+        }
+        console.log(newSum);
+
+        var diffArr = [];
+        for (i = 0; i < friendData.length; i++) {
+            
+            var friendSum = 0;
+            for (j = 0; j < friendData[i].scores.length; j++) {
+                friendSum += parseInt(friendData[i].scores[j]);
+            }
+
+            abDiff = Math.abs(newSum - friendSum);
+            diffArr.push(abDiff);
+        }
+        var lowDiff = Math.min(...diffArr);
+        var indi = diffArr.indexOf(lowDiff);
+
+        var foundResult = friendData[indi];
+
+        console.log(lowDiff);
+        console.log(diffArr);
+        console.log(indi);
+        console.log(foundResult);
+        
+        // Push newFriend into friendData
+        friendData.push(newFriend);
+        res.json(foundResult);
         console.log("Added to Group of friends!")
+
     })
 
 }
